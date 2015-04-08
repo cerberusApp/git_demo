fb963dd615a6afe70fb8b6f2ee96697c389d4c91
cerberusApp <luyang@asiainfo.com>
2015-04-08 14:22:20 +0800

    1.测试

--- src/demo/Demo.java
+++ src/demo/Demo.java
@@ -9,10 +9,13 @@
  */
 package demo;
 
+import java.util.Date;
+
 public class Demo
 {
 	public static void main(String[] args)
 	{
+		System.out.println(new Date());
 		System.out.println("test Demo");
 	}
 }
