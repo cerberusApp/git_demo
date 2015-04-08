729eef008fb06c4bca6abcf2ebc64dfd8541dbb8
cerberusApp <luyang@asiainfo.com>
2015-04-08 14:38:06 +0800

    demo02

---
+++ src/demo/Demo2.java
@@ -0,0 +1,18 @@
+/**
+ * @File: Demo2.java 
+ * @Package  demo
+ * @Description: 
+ * @author luyang
+ * @date 2015年4月8日 下午2:37:11 
+ * @version V1.0
+ * 
+ */
+package demo;
+
+public class Demo2
+{
+	public static void main(String[] args)
+	{
+		System.out.println("demo 02");
+	}
+}
