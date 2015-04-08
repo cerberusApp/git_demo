0db44db89675a307e003d3a024ffd450107473d3
cerberusApp <luyang@asiainfo.com>
2015-04-08 14:09:47 +0800

    1.测试首次提交

---
+++ src/demo/Demo.java
@@ -0,0 +1,18 @@
+/**
+ * @File: Demo.java 
+ * @Package  demo
+ * @Description: 
+ * @author luyang
+ * @date 2015年4月8日 下午2:07:01 
+ * @version V1.0
+ * 
+ */
+package demo;
+
+public class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("test Demo");
+	}
+}
